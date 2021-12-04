ff0d4625ffa68f3dcbb970bfa2e0113e138146db
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-04 13:13:32 +0900

    fix

--- model/heya.go
+++ model/heya.go
@@ -6,14 +6,15 @@ import (
 	"time"
 )
 
-type Heyas struct {
-	ID           uuid.UUID      `json:"id" gorm:"type:char(36);not null;primaryKey"`
-	Title        string         `json:"title" gorm:"type:char(50);not null"`
-	Description  string         `json:"description" gorm:"type:text;not null"`
-	CreatorID    uuid.UUID      `json:"creator_id" gorm:"type:char(36);not null"`
-	LastEditorID uuid.UUID      `json:"last_editor_id" gorm:"type:char(36);not null"`
-	CreatedAt    time.Time      `json:"created_at"     gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
-	UpdatedAt    time.Time      `json:"updated_at"     gorm:"type:DATETIME;default:CURRENT_TIMESTAMP"`
-	DeletedAt    gorm.DeletedAt `json:"deleted_at"     gorm:"type:DATETIME;default:NULL"`
-	CreatorUser        User          `json:"-" gorm:"foreignKey:ID"`
+type Heya struct {
+	ID             uuid.UUID      `json:"id"             gorm:"type:char(36);not null;primaryKey"`
+	Title          string         `json:"title"          gorm:"type:char(50);not null"`
+	Description    string         `json:"description"    gorm:"type:text;not null"`
+	CreatorID      uuid.UUID      `json:"creator_id"     gorm:"type:char(36);not null"`
+	LastEditorID   uuid.UUID      `json:"last_editor_id" gorm:"type:char(36);not null"`
+	CreatedAt      time.Time      `json:"created_at"     gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt      time.Time      `json:"updated_at"     gorm:"type:DATETIME;default:CURRENT_TIMESTAMP"`
+	DeletedAt      gorm.DeletedAt `json:"deleted_at"     gorm:"type:DATETIME;default:NULL"`
+	CreatorUser    *User          `json:"-"              gorm:"foreignKey:CreatorID"`
+	LastEditorUser *User          `json:"-"              gorm:"foreignKey:LastEditorID"`
 }
