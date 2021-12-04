551281934d9eddcbf3504edf078e291064cf7f4d
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-04 12:45:37 +0900

    imagesのモデル

---
+++ model/images.go
@@ -0,0 +1,8 @@
+package model
+
+import "github.com/gofrs/uuid"
+
+type Images struct {
+	ID    uuid.UUID `json:"id"    gorm:"type:char(36);not null;primaryKey"`
+	Image []byte    `json:"image" gorm:"type:MEDIUMBLOB;not null"`
+}
