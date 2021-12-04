7f3b7fed29ffd61d7073436ed55cc9a17a75fd01
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-04 13:19:24 +0900

    外部キーの追加

--- model/hiqidashi.go
+++ model/hiqidashi.go
@@ -7,15 +7,21 @@ import (
 )
 
 type Hiqidashi struct {
-	ID           uuid.UUID      `json:"id"             gorm:"type:char(36);not null;primaryKey"`
-	SheetID      uuid.UUID      `json:"sheet_id"       gorm:"type:char(36);not null;primaryKey"`
-	CreatorID    uuid.UUID      `json:"creator_id"     gorm:"type:char(36);not null"`
-	LastEditorID uuid.NullUUID  `json:"last_editor_id" gorm:"type:char(36)"`
-	ParentID     uuid.NullUUID  `json:"parent_id"      gorm:"type:char(36)"`
-	Title        string         `json:"title"          gorm:"type:char(50);not null"`
-	Description  string         `json:"description"    gorm:"type:text;not null"`
-	ImageID      uuid.UUID      `json:"image_id"       gorm:"type:char(36);not null"`
-	CreatedAt    time.Time      `json:"created_at"     gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
-	UpdatedAt    time.Time      `json:"updated_at"     gorm:"type:DATETIME;default:CURRENT_TIMESTAMP"`
-	DeletedAt    gorm.DeletedAt `json:"deleted_at"     gorm:"type:DATETIME;default:NULL"`
+	ID             uuid.UUID      `json:"id"             gorm:"type:char(36);not null;primaryKey"`
+	HeyaID         uuid.UUID      `json:"heya_id"       gorm:"type:char(36);not null;primaryKey"`
+	CreatorID      uuid.UUID      `json:"creator_id"     gorm:"type:char(36);not null"`
+	LastEditorID   uuid.NullUUID  `json:"last_editor_id" gorm:"type:char(36);not null"`
+	ParentID       uuid.NullUUID  `json:"parent_id"      gorm:"type:char(36);not null"`
+	Title          string         `json:"title"          gorm:"type:char(50);not null"`
+	Description    string         `json:"description"    gorm:"type:text;not null"`
+	ImageID        uuid.UUID      `json:"image_id"       gorm:"type:char(36);not null"`
+	CreatedAt      time.Time      `json:"created_at"     gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt      time.Time      `json:"updated_at"     gorm:"type:DATETIME;default:CURRENT_TIMESTAMP"`
+	DeletedAt      gorm.DeletedAt `json:"deleted_at"     gorm:"type:DATETIME;default:NULL"`
+	CreatorUser    *User          `json:"-"              gorm:"foreignKey:CreatorID"`
+	LastEditorUser *User          `json:"-"              gorm:"foreignKey:LastEditorID"`
+	Image          *Image         `json:"-"              gorm:"foreignKey:ImageID"`
+	Heya           *Heya          `json:"-"              gorm:"foreignKey:HeyaID"`
+
+	ChildrenID []uuid.UUID `gorm:"-"`
 }
