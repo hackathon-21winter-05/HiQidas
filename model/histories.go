42956982200faeee342c877c092510badd663e6e
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-05 10:54:41 +0900

    priorityの追加

--- model/histories.go
+++ model/histories.go
@@ -3,6 +3,6 @@ package model
 import "github.com/gofrs/uuid"
 
 type History struct {
-	UserID  uuid.UUID `json:"user_id"  gorm:"type:char(36);not null;primaryKey"`
-	SheetID uuid.UUID `json:"sheet_id" gorm:"type:char(36);not null;primaryKey"`
+	UserID  uuid.UUID `json:"user_id"  gorm:"type:char(36);not null;primaryKey;index:idx_history_user_id,priority:1"`
+	SheetID uuid.UUID `json:"sheet_id" gorm:"type:char(36);not null;primaryKey;index:idx_history_user_id,priority:2"`
 }
