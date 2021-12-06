82f35972e2a0e43ea76be4096bb1ca05da38b6a2
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-06 11:59:25 +0900

    fix

--- model/images.go
+++ model/images.go
@@ -4,5 +4,5 @@ import "github.com/gofrs/uuid"
 
 type Drawing struct {
 	ID      uuid.UUID `json:"id"    gorm:"type:char(36);not null;primaryKey"`
-	Drawing string    `json:"image" gorm:"type:text;not null"`
+	Drawing string    `json:"drawing" gorm:"type:text;not null"`
 }
