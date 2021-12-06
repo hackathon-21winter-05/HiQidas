2fc34ddddeb86d9464cbe0661a89e6ba4e68549b
logica <84759675+logica0419@users.noreply.github.com>
2021-12-07 05:09:08 +0900

    traQのクライアントをハンドラー群に追加

--- server/rest/rest.go
+++ server/rest/rest.go
@@ -1,6 +1,9 @@
 package rest
 
-import "github.com/hackathon-21winter-05/HiQidas/config"
+import (
+	"github.com/hackathon-21winter-05/HiQidas/config"
+	"github.com/sapphi-red/go-traq"
+)
 
 // REST APIハンドラー群
 type RestHandlers interface {
@@ -9,12 +12,14 @@ type RestHandlers interface {
 
 // REST APIハンドラー群 実装
 type restHandlersImpl struct {
-	c *config.Config
+	c   *config.Config
+	cli *traq.APIClient
 }
 
 // 新しいREST APIハンドラー群を生成
 func NewRestHandlers(c *config.Config) RestHandlers {
 	return &restHandlersImpl{
-		c: c,
+		c:   c,
+		cli: traq.NewAPIClient(traq.NewConfiguration()),
 	}
 }
