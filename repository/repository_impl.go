d072ee891ef9ffaa5f82c0d3f111a984e6b61573
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-08 00:22:58 +0900

    GormRepoのコンストラクタの追加

---
+++ repository/repository_impl.go
@@ -0,0 +1,12 @@
+package repository
+
+import "gorm.io/gorm"
+
+type GormRepository struct {
+	db *gorm.DB
+}
+
+func NewGormRepository(db *gorm.DB) *GormRepository {
+	return &GormRepository{db: db}
+}
+
