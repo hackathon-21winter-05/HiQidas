d6c621115bc34b65bcc1ef9bf0589e4a6ee249a0
logica <84759675+logica0419@users.noreply.github.com>
2021-12-08 03:43:12 +0900

    descriptionのをnot nullに

--- model/heya.go
+++ model/heya.go
@@ -1,19 +1,18 @@
 package model
 
 import (
-	"database/sql"
 	"time"
 
 	"github.com/gofrs/uuid"
 )
 
 type Heya struct {
-	ID           uuid.UUID      `gorm:"type:char(36);not null;primaryKey"`
-	Title        string         `gorm:"type:char(50);not null"`
-	Description  sql.NullString `gorm:"type:text"`
-	CreatorID    uuid.UUID      `gorm:"type:char(36);not null"`
-	LastEditorID uuid.UUID      `gorm:"type:char(36);not null"`
-	CreatedAt    time.Time      `gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
-	UpdatedAt    time.Time      `gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
-	Deleted      bool           `gorm:"type:boolean;default:false;not null"`
+	ID           uuid.UUID `gorm:"type:char(36);not null;primaryKey"`
+	Title        string    `gorm:"type:char(50);not null"`
+	Description  string    `gorm:"type:text;not null"`
+	CreatorID    uuid.UUID `gorm:"type:char(36);not null"`
+	LastEditorID uuid.UUID `gorm:"type:char(36);not null"`
+	CreatedAt    time.Time `gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt    time.Time `gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
+	Deleted      bool      `gorm:"type:boolean;default:false;not null"`
 }
