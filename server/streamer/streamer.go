be5fa1458d4df8d4602fa69b44dcc394615f0d1f
logica <84759675+logica0419@users.noreply.github.com>
2021-12-08 02:24:07 +0900

    ストリーマーを定義

---
+++ server/streamer/streamer.go
@@ -0,0 +1,32 @@
+package streamer
+
+import (
+	"log"
+
+	"github.com/gofrs/uuid"
+)
+
+type Streamer struct {
+	clients       map[uuid.UUID]*client
+	receiveBuffer chan *cliMessage
+}
+
+func NewStreamer() *Streamer {
+	s := &Streamer{
+		clients:       map[uuid.UUID]*client{},
+		receiveBuffer: make(chan *cliMessage),
+	}
+
+	return s
+}
+
+func (s *Streamer) listen() {
+	for {
+		msg := <-s.receiveBuffer
+		log.Print(*msg)
+	}
+}
+
+func (s *Streamer) Run() {
+	s.listen()
+}
