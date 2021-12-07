62c3ed289a03668579384e5600a2da87210ce92c
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-07 12:14:39 +0900

    errorを小文字にした

--- repository/database/errors.go
+++ repository/database/errors.go
@@ -4,9 +4,9 @@ import "errors"
 
 var (
 	// ErrNoRecordDeleted 削除するレコードがありませんでした
-	ErrNoRecordDeleted = errors.New("no Record Deleted")
+	ErrNoRecordDeleted = errors.New("no record deleted")
 	// ErrNoRecordUpdated 更新するレコードがありませんでした
-	ErrNoRecordUpdated = errors.New("no Record Updated")
+	ErrNoRecordUpdated = errors.New("no record updated")
 )
 
 
