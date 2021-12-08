4cc096a61a75332ee2af3fe62c807a2565b0a525
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-08 22:20:24 +0900

    fix

--- model/tsuna.go
+++ model/tsuna.go
@@ -4,6 +4,7 @@ import "github.com/gofrs/uuid"
 
 type Tsuna struct {
 	ID           uuid.UUID `gorm:"type:char(36);not null;primaryKey"`
+	HeyaID       uuid.UUID `gorm:"type:char(36);not null"`
 	HiqidashiOne uuid.UUID `gorm:"type:char(36);not null"`
 	HiqidashiTwo uuid.UUID `gorm:"type:char(36);not null"`
 }
--- repository/tsuna.go
+++ repository/tsuna.go
@@ -7,8 +7,8 @@ import (
 )
 
 type tsunaRepository interface {
-	GetTsuna(ctx context.Context, id uuid.UUID) (*model.Tsuna, error)
-	InsertTsuna(ctx context.Context, tsuna *model.Tsuna) error
+	GetTsunas(ctx context.Context, heyaID uuid.UUID) ([]*model.Tsuna, error)
+	CreateTsuna(ctx context.Context, tsuna *model.Tsuna) error
 	DeleteTsuna(ctx context.Context, tsuna *model.Tsuna) error
 	UpdateTsuna(Ctx context.Context, tsuna *model.Tsuna) error
 }
