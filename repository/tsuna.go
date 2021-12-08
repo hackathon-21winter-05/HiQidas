0e291a6594bdb4888d5e7acd30543f9838585993
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-08 22:22:36 +0900

    fix

--- repository/tsuna.go
+++ repository/tsuna.go
@@ -9,6 +9,6 @@ import (
 type tsunaRepository interface {
 	GetTsunas(ctx context.Context, heyaID uuid.UUID) ([]*model.Tsuna, error)
 	CreateTsuna(ctx context.Context, tsuna *model.Tsuna) error
-	DeleteTsuna(ctx context.Context, tsuna *model.Tsuna) error
-	UpdateTsuna(Ctx context.Context, tsuna *model.Tsuna) error
+	DeleteTsuna(ctx context.Context, id uuid.UUID) error
+	UpdateTsuna(Ctx context.Context, id uuid.UUID) error
 }
