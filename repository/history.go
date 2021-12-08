cf8cbc0fdfa39ede409413dc3fcc0747886456b1
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-08 20:12:22 +0900

    fix

--- repository/history.go
+++ repository/history.go
@@ -2,10 +2,11 @@ package repository
 
 import (
 	"context"
+	"github.com/gofrs/uuid"
 	"github.com/hackathon-21winter-05/HiQidas/model"
 )
 
 type HistoryRepository interface {
 	GetHistoriesByUserID(ctx context.Context) ([]*model.History, error)
-	DeleteHistory(ctx context.Context, heyaId context.Context) error
+	DeleteHistory(ctx context.Context, heyaId uuid.UUID) error
 }
