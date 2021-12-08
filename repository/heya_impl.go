2f84c03e3557ba901fcc5cce5a7287cc827a6bdb
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-09 02:46:44 +0900

    GetHeyasIDの実装

--- repository/heya_impl.go
+++ repository/heya_impl.go
@@ -3,12 +3,28 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"github.com/gofrs/uuid"
 	"github.com/hackathon-21winter-05/HiQidas/model"
 )
 
+// GetHeyasID すべてのヘヤのIDを取得
 func (repo *GormRepository) GetHeyasID(ctx context.Context) ([]uuid.UUID, error) {
-	panic("implement me")
+	db, err := repo.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db: %w", err)
+	}
+
+	var heyaIDs []uuid.UUID
+
+	err = db.
+		Model(model.Heya{}).
+		Pluck("id", &heyaIDs).Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to get heyaIDs :%w", err)
+	}
+
+	return heyaIDs, nil
 }
 
 func (repo *GormRepository) GetHeyaByID(ctx context.Context, id uuid.UUID) (*model.Heya, error) {
