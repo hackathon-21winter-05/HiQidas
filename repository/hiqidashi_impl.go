b52ca2d8ec202cd850437bc3df8418020650ced5
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-09 03:19:06 +0900

    GetHiqidashisByHeyaIDの実装

--- repository/hiqidashi_impl.go
+++ repository/hiqidashi_impl.go
@@ -2,12 +2,28 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"github.com/gofrs/uuid"
 	"github.com/hackathon-21winter-05/HiQidas/model"
 )
 
+// GetHiqidashisByHeyaID ヘヤのすべてのヒキダシを取得
 func (repo *GormRepository) GetHiqidashisByHeyaID(ctx context.Context, heyaID uuid.UUID) ([]*model.Hiqidashi, error) {
-	panic("implement me")
+	db, err := repo.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db: %w", err)
+	}
+
+	hiqidashis := make([]*model.Hiqidashi, 0)
+
+	err = db.
+		Where("heya_id = ?", heyaID).
+		Find(&hiqidashis).Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to get hiqidashi by heyaID :%w", err)
+	}
+
+	return hiqidashis, nil
 }
 
 func (repo *GormRepository) GetHiqidashisByParentID(ctx context.Context, parentID uuid.UUID) ([]*model.Hiqidashi, error) {
