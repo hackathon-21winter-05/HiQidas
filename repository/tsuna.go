091e4d5f11ff7a11b308e45bc0f3cd5521ecdf68
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-08 21:34:35 +0900

    tsunarepoのインターフェイス

--- repository/tsuna.go
+++ repository/tsuna.go
@@ -2,11 +2,13 @@ package repository
 
 import (
 	"context"
+	"github.com/gofrs/uuid"
 	"github.com/hackathon-21winter-05/HiQidas/model"
 )
 
 type tsunaRepository interface {
-	GetTsuna(ctx context.Context) (*model.Tsuna, error)
+	GetTsuna(ctx context.Context, id uuid.UUID) (*model.Tsuna, error)
 	InsertTsuna(ctx context.Context, tsuna *model.Tsuna) error
-	DeleteTsuna(ctx context.Context,tsuna *model.Tsuna) error
+	DeleteTsuna(ctx context.Context, tsuna *model.Tsuna) error
+	UpdateTsuna(Ctx context.Context, tsuna *model.Tsuna) error
 }
