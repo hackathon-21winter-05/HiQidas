32f2ee1ee9b26c049050079727cec43325d54513
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-08 20:20:42 +0900

    InsertとGetを作成

--- repository/history.go
+++ repository/history.go
@@ -7,6 +7,8 @@ import (
 )
 
 type HistoryRepository interface {
-	GetHistoriesByUserID(ctx context.Context,userID uuid.UUID) ([]*model.History, error)
+	GetHistories(ctx context.Context) ([]*model.History, error)
+	GetHistoriesByUserID(ctx context.Context, userID uuid.UUID) ([]*model.History, error)
+	CreateHistory(ctx context.Context, heyaID uuid.UUID) error //部屋が作成されたらこれも叩く
 	DeleteHistory(ctx context.Context, heyaID uuid.UUID) error
 }
