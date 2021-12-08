ddc12b5d34d9ac8ece69aaca170d42d0bb98080d
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-09 02:25:03 +0900

    Ctx -> ctx

--- repository/tsuna.go
+++ repository/tsuna.go
@@ -10,5 +10,5 @@ type TsunaRepository interface {
 	GetTsunasByHeyaID(ctx context.Context, heyaID uuid.UUID) ([]*model.Tsuna, error)
 	CreateTsuna(ctx context.Context, tsuna *model.Tsuna) error
 	DeleteTsuna(ctx context.Context, id uuid.UUID) error
-	UpdateTsuna(Ctx context.Context, tsuna *model.Tsuna) error
+	UpdateTsuna(ctx context.Context, tsuna *model.Tsuna) error
 }
