7519d0c43c36d349fab976a6e5196ca2c25297a1
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-08 21:29:32 +0900

    tsunaのレポ

--- repository/tsuna.go
+++ repository/tsuna.go
@@ -1,5 +1,12 @@
 package repository
 
-type tsunaRepository interface {
+import (
+	"context"
+	"github.com/hackathon-21winter-05/HiQidas/model"
+)
 
+type tsunaRepository interface {
+	GetTsuna(ctx context.Context) (*model.Tsuna, error)
+	InsertTsuna(ctx context.Context, tsuna *model.Tsuna) error
+	DeleteTsuna(ctx context.Context,tsuna *model.Tsuna) error
 }
