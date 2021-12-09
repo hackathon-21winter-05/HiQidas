285af0a7af23297daf1d0e121da0447a76971815
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-10 04:25:45 +0900

    DeleteHeyaの実装

--- service/heya/heya.go
+++ service/heya/heya.go
@@ -2,6 +2,8 @@ package heya
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/gofrs/uuid"
 	"github.com/hackathon-21winter-05/HiQidas/model"
 	"github.com/hackathon-21winter-05/HiQidas/repository"
@@ -11,10 +13,37 @@ import (
 
 type HeyaService interface {
 	CreateHeya(userID uuid.UUID, title, description string) (*model.Heya, error)
+	DeleteHeya(heyaID uuid.UUID) error
 }
 
 type HeyaServiceImpl struct {
 	repo repository.Repository
+}
+
+func (h *HeyaServiceImpl) DeleteHeya(heyaID uuid.UUID) error {
+	ctx, cancel := utils.CreateTxContext()
+	defer cancel()
+
+	err := h.repo.Do(ctx, nil, func(ctx context.Context) error {
+		if err := h.repo.DeleteHeyaByID(ctx, heyaID); err != nil {
+			return err
+		}
+		if err := h.repo.DeleteHistoryByHeyaID(ctx, heyaID); err != nil {
+			return err
+		}
+		if err := h.repo.DeleteHiqidashiByHeyaID(ctx, heyaID); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		if errors.Is(err, repository.ErrNoRecordDeleted) {
+			return fmt.Errorf("failed to delete no record")
+		}
+		return err
+	}
+
+	return nil
 }
 
 func (h *HeyaServiceImpl) CreateHeya(userID uuid.UUID, title, description string) (*model.Heya, error) {
