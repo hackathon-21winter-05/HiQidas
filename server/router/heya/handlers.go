9762460fb3248a3b9a438fd5efdaeac4855b1eeb
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-10 03:30:32 +0900

    PostHeyasHandlerの実装

--- server/router/heya/handlers.go
+++ server/router/heya/handlers.go
@@ -1,6 +1,7 @@
 package heya
 
 import (
+	"github.com/gofrs/uuid"
 	"github.com/hackathon-21winter-05/HiQidas/server/protobuf/rest"
 	"github.com/hackathon-21winter-05/HiQidas/server/router/utils"
 	"github.com/labstack/echo/v4"
@@ -26,16 +27,30 @@ func (h *HeyaHandleGroup) DeleteHeyasByIDHandler(c echo.Context) error {
 func (h *HeyaHandleGroup) PostHeyasHandler(c echo.Context) error {
 	heyaRequest := rest.PostHeyasRequest{}
 
-	if err:=utils.BindProtobuf(c,&heyaRequest); err!=nil {
+	if err := utils.BindProtobuf(c, &heyaRequest); err != nil {
 		c.Logger().Info(err)
-		return echo.NewHTTPError(http.StatusBadRequest,err)
+		return echo.NewHTTPError(http.StatusBadRequest, err)
 	}
 
-	//service層にデータを加工してもらう
-	heyaResponse := rest.PostHeyasResponse{}
-	heya,err := h.hs.SaveHeya(heyaRequest.Title,heyaRequest.Description)
-
-	return utils.SendProtobuf(c,http.StatusCreated,&heyaResponse)
+	//TODO:セッションからUserIDをもってこれるようにする
+	heya, err := h.hs.CreateHeya(uuid.Nil, heyaRequest.Title, heyaRequest.Description)
+	if err != nil {
+		c.Logger().Error(err)
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+	rheya := rest.Heya{
+		Id:           heya.ID.String(),
+		Title:        heya.Title,
+		Description:  heya.Description,
+		CreatorId:    heya.CreatorID.String(),
+		LastEditorId: heya.LastEditorID.String(),
+		CreatedAt:    utils.TimeStampToTIme(heya.CreatedAt),
+		UpdatedAt:    utils.TimeStampToTIme(heya.UpdatedAt),
+	}
+	heyaResponse := rest.PostHeyasResponse{
+		Heya: &rheya,
+	}
+	return utils.SendProtobuf(c, http.StatusCreated, &heyaResponse)
 }
 
 func (h *HeyaHandleGroup) PutHeyasByIDHandler(c echo.Context) error {
