04cae6aa6cf3c0cca27bcdeab5b403a02c74c7b5
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-11 02:32:14 +0900

    serviceを整理

--- service/user.go
+++ service/user.go
@@ -4,23 +4,14 @@ import (
 	"context"
 
 	"github.com/hackathon-21winter-05/HiQidas/model"
-	"github.com/hackathon-21winter-05/HiQidas/repository"
 )
 
 type UserService interface {
 	GetUsersID() (model.UserIDs, error)
 }
 
-type UserServiceImpl struct {
-	repo repository.Repository
-}
-
-func NewUserServiceImpl(repo repository.Repository) *UserServiceImpl {
-	return &UserServiceImpl{repo: repo}
-}
-
-func (us *UserServiceImpl) GetUsersID() (model.UserIDs, error) {
-	userIDs, err := us.repo.GetUsersID(context.Background())
+func (s *Service) GetUsersID() (model.UserIDs, error) {
+	userIDs, err := s.repo.GetUsersID(context.Background())
 	if err != nil {
 		return nil, err
 	}
