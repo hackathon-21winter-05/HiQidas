b27a7e70c8ca67f2bf2e3d47694cd9b66a122868
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-11 02:31:07 +0900

    serviceを整理

--- service/service.go
+++ service/service.go
@@ -1,17 +1,12 @@
 package service
 
 import (
-	"context"
-	"github.com/gofrs/uuid"
-	"github.com/hackathon-21winter-05/HiQidas/model"
 	"github.com/hackathon-21winter-05/HiQidas/repository"
 )
 
 type Service struct {
 	repo repository.Repository
 }
-
-
 
 func NewService(repo repository.Repository) *Service {
 	return &Service{repo: repo}
