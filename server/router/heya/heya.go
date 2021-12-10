5469b104faa260ec80eff3eb6e60cc70557f3036
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-10 17:17:45 +0900

    はんどらの削除

--- server/router/heya/heya.go
+++ server/router/heya/heya.go
@@ -2,7 +2,6 @@ package heya
 
 import (
 	"github.com/hackathon-21winter-05/HiQidas/service/heya"
-	"github.com/labstack/echo/v4"
 )
 
 type HeyaHandleGroup struct {
@@ -11,13 +10,4 @@ type HeyaHandleGroup struct {
 
 func NewHeyaHandleGroup(hs heya.HeyaService) *HeyaHandleGroup {
 	return &HeyaHandleGroup{hs: hs}
-}
-
-func (h *HeyaHandleGroup) Setup(heyasApi *echo.Group) {
-	heyasApi.GET("",h.GetHeyasHandler)
-	heyasApi.GET("/:heyaID",h.GetHeyasByIDHandler)
-	heyasApi.GET("/:heyaID/users",h.GetUsersByHeyaIDHandler)
-	heyasApi.POST("",h.PostHeyasHandler)
-	heyasApi.PUT("/:heyaID",h.PutHeyasByIDHandler)
-	heyasApi.DELETE("/:heyaID",h.DeleteHeyasByIDHandler)
 }
