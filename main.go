5a63d94882235a5df44daaf9ae05099f0d0c82ec
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-10 16:47:16 +0900

    configの追加

--- main.go
+++ main.go
@@ -5,9 +5,7 @@ import (
 	"time"
 
 	"github.com/hackathon-21winter-05/HiQidas/config"
-	"github.com/hackathon-21winter-05/HiQidas/repository"
 	"github.com/hackathon-21winter-05/HiQidas/server"
-	"github.com/hackathon-21winter-05/HiQidas/service"
 )
 
 func init() {
@@ -28,14 +26,7 @@ func main() {
 		log.Panic(err)
 	}
 
-	repo, err := repository.NewGormRepository(c)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	ser := service.NewService(repo)
-
-	s := server.NewServer(c, ser)
+	s := server.NewServer(c)
 
 	s.Run()
 }
