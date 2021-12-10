91d65fe0a7cd4497d4d1df4fcb8b7dadcf4c4a3d
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-11 03:19:17 +0900

    wireを生成

---
+++ server/wire.go
@@ -0,0 +1,40 @@
+//+build wireinject
+
+package server
+
+import (
+	"github.com/google/wire"
+	"github.com/hackathon-21winter-05/HiQidas/config"
+	"github.com/hackathon-21winter-05/HiQidas/repository"
+	"github.com/hackathon-21winter-05/HiQidas/server/router"
+	"github.com/hackathon-21winter-05/HiQidas/server/router/heya"
+	"github.com/hackathon-21winter-05/HiQidas/server/router/middleware"
+	"github.com/hackathon-21winter-05/HiQidas/server/router/oauth"
+	"github.com/hackathon-21winter-05/HiQidas/server/router/user"
+	"github.com/hackathon-21winter-05/HiQidas/server/router/ws"
+	"github.com/hackathon-21winter-05/HiQidas/server/streamer"
+	"github.com/hackathon-21winter-05/HiQidas/service"
+	"github.com/sapphi-red/go-traq"
+)
+
+var SuperSet = wire.NewSet(
+	repository.NewGormRepository,
+	wire.Struct(new(repository.GormRepository), "*"),
+	service.NewService,
+
+	user.NewUserHandlerGroup,
+	heya.NewHeyaHandleGroup,
+	oauth.NewOauthHandlerGroup,
+	wire.NewSet(traq.NewAPIClient, traq.NewConfiguration),
+	middleware.NewMiddleware,
+	ws.NewWSHandlerGroup,
+	streamer.NewStreamer,
+	router.NewAPIHandler,
+	router.NewRouter,
+	NewServer,
+)
+
+func InjectStream(c *config.Config) (*Server, error) {
+	wire.Build(SuperSet)
+	return nil, nil
+}
