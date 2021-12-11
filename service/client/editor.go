9caa088abd87ce057b01413bcf27dd136f8c72a6
logica <84759675+logica0419@users.noreply.github.com>
2021-12-11 23:06:05 +0900

    Mutexの記法の改善

--- service/client/editor.go
+++ service/client/editor.go
@@ -6,28 +6,37 @@ import (
 	"github.com/gofrs/uuid"
 )
 
-var editorClients = map[uuid.UUID][]uuid.UUID{}
-var editorClientsMutex = &sync.RWMutex{}
+type editorClientsMap struct {
+	cli map[uuid.UUID][]uuid.UUID
+	sync.RWMutex
+}
+
+var editorClients = editorClientsMap{
+	cli: make(map[uuid.UUID][]uuid.UUID),
+}
 
 func (cs *ClientServiceImpl) GetEditorClientsIDByHiqidashiID(hiqidashiID uuid.UUID) []uuid.UUID {
-	editorClientsMutex.RLock()
-	defer editorClientsMutex.RUnlock()
-	return editorClients[hiqidashiID]
+	editorClients.RLock()
+	defer editorClients.RUnlock()
+
+	return editorClients.cli[hiqidashiID]
 }
 
 func (cs *ClientServiceImpl) AddEditorClient(hiqidashiID uuid.UUID, clientID uuid.UUID) {
-	editorClientsMutex.Lock()
-	defer editorClientsMutex.Unlock()
-	editorClients[hiqidashiID] = append(editorClients[hiqidashiID], clientID)
+	editorClients.Lock()
+	defer editorClients.Unlock()
+
+	editorClients.cli[hiqidashiID] = append(editorClients.cli[hiqidashiID], clientID)
 }
 
 func (cs *ClientServiceImpl) DeleteEditorClient(hiqidashiID uuid.UUID, clientID uuid.UUID) error {
-	for i, v := range editorClients[hiqidashiID] {
+	editorClients.Lock()
+	defer editorClients.Unlock()
+
+	for i, v := range editorClients.cli[hiqidashiID] {
 		if v == clientID {
-			editorClientsMutex.Lock()
-			defer editorClientsMutex.Unlock()
-			editorClients[hiqidashiID][i] = editorClients[hiqidashiID][len(editorClients[hiqidashiID])-1]
-			editorClients[hiqidashiID] = editorClients[hiqidashiID][:len(editorClients[hiqidashiID])-1]
+			editorClients.cli[hiqidashiID][i] = editorClients.cli[hiqidashiID][len(editorClients.cli[hiqidashiID])-1]
+			editorClients.cli[hiqidashiID] = editorClients.cli[hiqidashiID][:len(editorClients.cli[hiqidashiID])-1]
 			return nil
 		}
 	}
--- service/client/heya.go
+++ service/client/heya.go
@@ -6,28 +6,37 @@ import (
 	"github.com/gofrs/uuid"
 )
 
-var heyaClients = map[uuid.UUID][]uuid.UUID{}
-var heyaClientsMutex = &sync.RWMutex{}
+type heyaClientsMap struct {
+	cli map[uuid.UUID][]uuid.UUID
+	sync.RWMutex
+}
+
+var heyaClients = heyaClientsMap{
+	cli: make(map[uuid.UUID][]uuid.UUID),
+}
 
 func (cs *ClientServiceImpl) GetHeyaClientsIDByHeyaID(heyaID uuid.UUID) []uuid.UUID {
-	heyaClientsMutex.RLock()
-	defer heyaClientsMutex.RUnlock()
-	return heyaClients[heyaID]
+	heyaClients.RLock()
+	defer heyaClients.RUnlock()
+
+	return heyaClients.cli[heyaID]
 }
 
 func (cs *ClientServiceImpl) AddHeyaClient(heyaID uuid.UUID, clientID uuid.UUID) {
-	heyaClientsMutex.Lock()
-	defer heyaClientsMutex.Unlock()
-	heyaClients[heyaID] = append(heyaClients[heyaID], clientID)
+	heyaClients.Lock()
+	defer heyaClients.Unlock()
+
+	heyaClients.cli[heyaID] = append(heyaClients.cli[heyaID], clientID)
 }
 
 func (cs *ClientServiceImpl) DeleteHeyaClient(heyaID uuid.UUID, clientID uuid.UUID) error {
-	for i, v := range heyaClients[heyaID] {
+	heyaClients.Lock()
+	defer heyaClients.Unlock()
+
+	for i, v := range heyaClients.cli[heyaID] {
 		if v == clientID {
-			heyaClientsMutex.Lock()
-			defer heyaClientsMutex.Unlock()
-			heyaClients[heyaID][i] = heyaClients[heyaID][len(heyaClients[heyaID])-1]
-			heyaClients[heyaID] = heyaClients[heyaID][:len(heyaClients[heyaID])-1]
+			heyaClients.cli[heyaID][i] = heyaClients.cli[heyaID][len(heyaClients.cli[heyaID])-1]
+			heyaClients.cli[heyaID] = heyaClients.cli[heyaID][:len(heyaClients.cli[heyaID])-1]
 			return nil
 		}
 	}
