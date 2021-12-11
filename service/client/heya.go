55db311942783f1ce3e1df2b0e77a012dae5fc36
reyu <ka15sugar@gmail.com>
2021-12-11 17:51:20 +0900

    bug fix

--- service/client/heya.go
+++ service/client/heya.go
@@ -1,24 +1,33 @@
 package client
 
 import (
+	"sync"
+
 	"github.com/gofrs/uuid"
 )
 
 var heyaClients = map[uuid.UUID][]uuid.UUID{}
+var heyaClientsMutex = &sync.RWMutex{}
 
 func (cs *ClientServiceImpl) GetHeyaClientsIDByHeyaID(heyaID uuid.UUID) []uuid.UUID {
+	heyaClientsMutex.RLock()
+	defer heyaClientsMutex.RUnlock()
 	return heyaClients[heyaID]
 }
 
 func (cs *ClientServiceImpl) AddHeyaClient(heyaID uuid.UUID, clientID uuid.UUID) {
+	heyaClientsMutex.Lock()
+	defer heyaClientsMutex.Unlock()
 	heyaClients[heyaID] = append(heyaClients[heyaID], clientID)
 }
 
 func (cs *ClientServiceImpl) DeleteHeyaClient(heyaID uuid.UUID, clientID uuid.UUID) error {
 	for i, v := range heyaClients[heyaID] {
 		if v == clientID {
-			heyaClients[heyaID][i] = heyaClients[heyaID][len(heyaClients)-1]
-			heyaClients[heyaID] = heyaClients[heyaID][:len(heyaClients)-1]
+			heyaClientsMutex.Lock()
+			defer heyaClientsMutex.Unlock()
+			heyaClients[heyaID][i] = heyaClients[heyaID][len(heyaClients[heyaID])-1]
+			heyaClients[heyaID] = heyaClients[heyaID][:len(heyaClients[heyaID])-1]
 			return nil
 		}
 	}
