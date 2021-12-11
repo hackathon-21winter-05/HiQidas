15c60e78897009f855a21af017ad7bf27396c191
logica <84759675+logica0419@users.noreply.github.com>
2021-12-12 00:56:37 +0900

    サービスを新モデルを使って実装

--- service/client/client.go
+++ service/client/client.go
@@ -4,13 +4,14 @@ import (
 	"errors"
 
 	"github.com/gofrs/uuid"
+	"github.com/hackathon-21winter-05/HiQidas/model"
 )
 
 var ErrNotFound = errors.New("client not found")
 
 type ClientService interface {
-	GetHeyaClientsIDByHeyaID(heyaID uuid.UUID) []uuid.UUID
-	AddHeyaClient(heyaID uuid.UUID, clientID uuid.UUID)
+	GetHeyaClientsIDByHeyaID(heyaID uuid.UUID) model.HeyaClients
+	AddHeyaClient(heyaID uuid.UUID, client model.HeyaClient)
 	DeleteHeyaClient(heyaID uuid.UUID, clientID uuid.UUID) error
 
 	GetEditorClientsIDByHiqidashiID(hiqidashiID uuid.UUID) []uuid.UUID
