35e9f1ca7fe90f969b78e5d89a84a3fec3f08450
logica <84759675+logica0419@users.noreply.github.com>
2021-12-28 03:54:38 +0900

    ヒストリーにアソシエーションを追加

--- model/history.go
+++ model/history.go
@@ -4,5 +4,7 @@ import "github.com/gofrs/uuid"
 
 type History struct {
 	UserID uuid.UUID `gorm:"type:char(36);not null;primaryKey;index:idx_history_user_id,priority:1"`
+	User   User      `gorm:"foreignKey:UserID;references:ID"`
 	HeyaID uuid.UUID `gorm:"type:char(36);not null;primaryKey;index:idx_history_user_id,priority:2"`
+	Heya   Heya      `gorm:"foreignKey:HeyaID;references:ID"`
 }
