29f2131384f655d0afe08d71b44669bb699b1dc9
logica <84759675+logica0419@users.noreply.github.com>
2021-12-28 04:00:14 +0900

    ヒキダシにアソシエーションを追加

--- model/hiqidashi.go
+++ model/hiqidashi.go
@@ -10,9 +10,13 @@ import (
 type Hiqidashi struct {
 	ID           uuid.UUID      `gorm:"type:char(36);not null;primaryKey"`
 	HeyaID       uuid.UUID      `gorm:"type:char(36);not null;index:idx_hiqidashi_heya_id,priority:1"`
+	Heya         Heya           `gorm:"foreignKey:HeyaID;references:ID"`
 	CreatorID    uuid.UUID      `gorm:"type:char(36);not null;index:idx_hiqidashi_creator_id,priority:1"`
+	Creator      User           `gorm:"foreignKey:CreatorID;references:ID"`
 	LastEditorID uuid.UUID      `gorm:"type:char(36);not null"`
+	LastEditor   User           `gorm:"foreignKey:LastEditorID;references:ID"`
 	ParentID     uuid.NullUUID  `gorm:"type:char(36)"`
+	Parent       *Hiqidashi     `gorm:"foreignKey:ParentID;references:ID"`
 	Title        string         `gorm:"type:char(50);not null"`
 	Description  string         `gorm:"type:text;not null"`
 	Drawing      sql.NullString `gorm:"type:text"`
