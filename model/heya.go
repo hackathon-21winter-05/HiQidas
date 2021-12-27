871c3beb8d3a01ca7bcdbf92ddb85d8ab314aeb5
logica <84759675+logica0419@users.noreply.github.com>
2021-12-28 04:00:26 +0900

    ヘヤにアソシエーションを追加

--- model/heya.go
+++ model/heya.go
@@ -14,6 +14,7 @@ type Heya struct {
 	CreatorID    uuid.UUID `gorm:"type:char(36);not null"`
 	Creator      User      `gorm:"foreignKey:CreatorID;references:ID"`
 	LastEditorID uuid.UUID `gorm:"type:char(36);not null"`
+	LastEditor   User      `gorm:"foreignKey:LastEditorID;references:ID"`
 	CreatedAt    time.Time `gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
 	UpdatedAt    time.Time `gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
 }
